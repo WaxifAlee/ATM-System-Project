9c1244c7c9c5f47cf2b28be2c76acad8bf5c282e
= <=>
2021-12-14 19:23:16 +0500

    created the main widget and credits for the developers

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 import tkinter as tk
 from tkinter import filedialog
+import time as t
+import os
+
+# Creating the main GUI Structure i.e. Root of the program
+os.system("cls")
+print("+---------------- H E L L O  T H E R E ! -----------------+\n")
+t.sleep(0.2)
+print("+--- This Program Was Created By Wasif Ali & Marium Ilyas ---+\n")
+input('Press Any Key To Continue...')
+
+root = tk.Tk()
+
+
+
+root.mainloop()
